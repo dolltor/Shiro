17b8fe13c5b04b1257822a0a29d53d877371cc45
dolltor <35116311+dolltor@users.noreply.github.com>
2018-01-04 20:38:15 -0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,8 +6,7 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-  if (message.content === '$ping')
-  {
+  if (message.content === '$ping') {
     message.reply('pong');
   }
 });
